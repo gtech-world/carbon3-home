7f9ff3dacfa87e2cc2ff611e810cac7ce31619b4
XueMoMo <XueMoMo@users.noreply.github.com>
2023-02-09 09:33:58 +0000

    deploy: 12e73e9b519d96345bec82c9f54cd02a84067654

---
+++ _next/static/pUjBa9qaxkjKuXTQUCan6/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-e3be577752de50a3.js"],"/_error":["static/chunks/pages/_error-82a61e6508dae5a8.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
