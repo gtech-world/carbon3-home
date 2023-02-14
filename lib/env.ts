e1dd85be470b78c903943e4caec112f8de750324
eericxu <2681350846@qq.com>
2023-02-14 14:11:27 +0800

    fix github pages env

--- lib/env.ts
+++ lib/env.ts
@@ -1,4 +1,4 @@
 
-export const API_BASE = process.env.NEXT_PUBLIC_API_BASE;
+export const API_BASE = process.env.NEXT_PUBLIC_API_BASE || "https://beta-api.gtech.world";
 
 console.info('API_BASE:', API_BASE)
