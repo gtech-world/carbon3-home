9aa00eb9d328ed10925a7224c5ac4a7b0c84f67c
eericxu <2681350846@qq.com>
2023-03-15 14:40:16 +0800

    optimize code

--- lib/hooks/useAutoAnim.ts
+++ lib/hooks/useAutoAnim.ts
@@ -6,38 +6,34 @@ export const AutoAnimPlugins: { [k in AutoAnimPluginsKeys]: AutoAnimationPlugin 
   "t-side": (el, action) => {
     return new KeyframeEffect(
       el,
-      [
-        ...(action === "add"
-          ? [
-              { transform: "scaleY(0)", transformOrigin: "0 0", opacity: 0 },
-              { transform: "scaleY(1)", transformOrigin: "0 0", opacity: 1 },
-            ]
-          : action === "remove"
-          ? [
-              { transform: "scaleY(1)", transformOrigin: "0 0", opacity: 1 },
-              { transform: "scaleY(0)", transformOrigin: "0 0", opacity: 0 },
-            ]
-          : []),
-      ],
+      action === "add"
+        ? [
+            { transform: "scaleY(0)", transformOrigin: "0 0", opacity: 0 },
+            { transform: "scaleY(1)", transformOrigin: "0 0", opacity: 1 },
+          ]
+        : action === "remove"
+        ? [
+            { transform: "scaleY(1)", transformOrigin: "0 0", opacity: 1 },
+            { transform: "scaleY(0)", transformOrigin: "0 0", opacity: 0 },
+          ]
+        : [],
       { duration: 300, easing: "ease-in-out" }
     );
   },
   scale: (el, action) => {
     return new KeyframeEffect(
       el,
-      [
-        ...(action === "add"
-          ? [
-              { transform: "scale(0.8)", opacity: 0 },
-              { transform: "scale(1)", opacity: 1 },
-            ]
-          : action === "remove"
-          ? [
-              { transform: "scale(1)", opacity: 1 },
-              { transform: "scale(0.8)", opacity: 0 },
-            ]
-          : []),
-      ],
+      action === "add"
+        ? [
+            { transform: "scale(0.8)", opacity: 0 },
+            { transform: "scale(1)", opacity: 1 },
+          ]
+        : action === "remove"
+        ? [
+            { transform: "scale(1)", opacity: 1 },
+            { transform: "scale(0.8)", opacity: 0 },
+          ]
+        : [],
       { duration: 300, easing: "ease-in-out" }
     );
   },
