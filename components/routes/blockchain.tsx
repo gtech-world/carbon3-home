8343eaf84a999ef47f42656db85383b318e2bd1d
lijun4639 <346766377@qq.com>
2023-03-31 14:16:54 +0800

    updated blockchain header

--- components/routes/blockchain.tsx
+++ components/routes/blockchain.tsx
@@ -18,28 +18,28 @@ import moment from 'moment'
 
 function ItemInfo(p: { label: string; text: string; link?: string; tip?: any; className?:string }) {
   return (
-    <div
-      className={classNames(
-        "w-full text-base whitespace-normal leading-[1.8rem] mo:text-[.9375rem] mo:leading-[1.6875rem] relative",
+      <div
+          className={classNames(
+              "w-full text-base whitespace-normal leading-[1.8rem] mo:text-[.9375rem] mo:leading-[1.6875rem] relative",
+              {
+                "text-green-2": p.link,
+                "text-gray-6": !p.link,
+              }
+              ,p.className)}
+      >
         {
-          "text-green-2": p.link,
-          "text-gray-6": !p.link,
+            !!p.tip &&
+            <VscQuestion data-tooltip-id="tooltip" data-tooltip-content={p.tip} className="absolute text-black left-[-1.6rem] text-xl top-[0.29rem]" />
         }
-      ,p.className)}
-    >
-      {
-        !!p.tip &&
-        <VscQuestion data-tooltip-id="tooltip" data-tooltip-content={p.tip} className="absolute text-black left-[-1.6rem] text-xl top-[0.29rem]" />
-      }
-      <span className="text-black font-bold">{p.label}:</span>{" "}
-      {p.link ? (
-        <a href={p.link} target="_blank" rel="noreferrer">
-          {p.text}
-        </a>
-      ) : (
-        p.text
-      )}
-    </div>
+        <span className="text-black font-bold">{p.label}:</span>{" "}
+        {p.link ? (
+            <a href={p.link} target="_blank" rel="noreferrer">
+              {p.text}
+            </a>
+        ) : (
+            p.text
+        )}
+      </div>
   );
 }
 interface LabelDetail{
@@ -61,17 +61,17 @@ function CardInfo(p: LabelDetail){
   })
   const { t } = useTranslation();
   return(
-    <div className="break-all">
-      <ItemInfo label={t("SBT Token ID")} text={data.sbtTokenId} link={genScanTokenUrl(data.sbtTokenId)} tip={t('SBTs are non-transferable and immutable digital credentials representing the vehicle’s carbon footprint label. Every SBT has a unique token ID.')} />
-      <ItemInfo label={t("Label No.")} text={data.labelNumber} />
-      <ItemInfo label={t("Label Owner")} text={data.productName} />
-      <ItemInfo className="mo:flex mo:flex-col" label={t("Owner Address")} text={data.ownerAddress} link={genScanUrl('address',data.ownerAddress)} tip={t('Trust Labels are minted by their owners. Owner Addresses stand for verified identity credentials for the label owners.')} />
-      <ItemInfo label={t("Product Model")} text={obj['Product Model']} />
-      <ItemInfo className="mo:flex mo:flex-col" label={t("Product Unique Identifier")} text={obj['VIN']} link={`/car?vin=${obj['VIN']}`} tip={t('The unique identifier number or code for the labelled product, typically a VIN code for the vehicle.')} />
-      <ItemInfo label={t("Label Type")} text={obj['Label Type']} tip={t('PCF Type stands for Product Carbon Footprint.')} />
-      <ItemInfo label={t("Data Scope")} text={'Cradle-to-Grave'} tip={t('The life cycle scope that the PCF data covers, typically Cradle-to-Grave (full-life-cycle) or Cradle-to-Gate.')} />
-      <ItemInfo label={t("Print Timestamp")} text={moment(data.labelPrintDate*1000).format('YYYY-MM-DD HH:mm:ss')} />
-    </div>
+      <div className="break-all">
+        <ItemInfo label={t("SBT Token ID")} text={data.sbtTokenId} link={genScanTokenUrl(data.sbtTokenId)} tip={t('SBTs are non-transferable and immutable digital credentials representing the vehicle’s carbon footprint label. Every SBT has a unique token ID.')} />
+        <ItemInfo label={t("Label No.")} text={data.labelNumber} />
+        <ItemInfo label={t("Label Owner")} text={data.productName} />
+        <ItemInfo className="mo:flex mo:flex-col" label={t("Owner Address")} text={data.ownerAddress} link={genScanUrl('address',data.ownerAddress)} tip={t('Trust Labels are minted by their owners. Owner Addresses stand for verified identity credentials for the label owners.')} />
+        <ItemInfo label={t("Product Model")} text={obj['Product Model']} />
+        <ItemInfo className="mo:flex mo:flex-col" label={t("Product Unique Identifier")} text={obj['VIN']} link={`/car?vin=${obj['VIN']}`} tip={t('The unique identifier number or code for the labelled product, typically a VIN code for the vehicle.')} />
+        <ItemInfo label={t("Label Type")} text={obj['Label Type']} tip={t('PCF Type stands for Product Carbon Footprint.')} />
+        <ItemInfo label={t("Data Scope")} text={'Cradle-to-Grave'} tip={t('The life cycle scope that the PCF data covers, typically Cradle-to-Grave (full-life-cycle) or Cradle-to-Gate.')} />
+        <ItemInfo label={t("Print Timestamp")} text={moment(data.labelPrintDate*1000).format('YYYY-MM-DD HH:mm:ss')} />
+      </div>
   )
 }
 
@@ -82,8 +82,8 @@ export function Blockchain() {
   const { t } = useTranslation();
   const isMobile = useIsMobile();
   const { value, loading }:any = useAsyncM(
-    noArgs(() => getSbtDetail(tokenId), [tokenId]),
-    [tokenId]
+      noArgs(() => getSbtDetail(tokenId), [tokenId]),
+      [tokenId]
   );
   const columns = [
     {
@@ -92,7 +92,7 @@ export function Blockchain() {
       tip: t('The cryptographic hash of an on-chain activity can be used to identify and verify the transaction information on the blockchain.'),
       render: (text:string)=>{
         return(
-          <Link className="text-green-2" target="_blank" href={genScanUrl('tx',text)}>{shortStr(text)}</Link>
+            <Link className="text-green-2" target="_blank" href={genScanUrl('tx',text)}>{shortStr(text)}</Link>
         )
       }
     },
@@ -113,7 +113,7 @@ export function Blockchain() {
       dataIndex: 'fromAddress',
       render: (text:string)=>{
         return(
-          <Link className="text-green-2" target="_blank" href={genScanUrl('address',text)}>{shortStr(text)}</Link>
+            <Link className="text-green-2" target="_blank" href={genScanUrl('address',text)}>{shortStr(text)}</Link>
         )
       }
     },
@@ -122,86 +122,93 @@ export function Blockchain() {
       dataIndex: 'toAddress',
       render: (text:string)=>{
         return(
-          <Link className="text-green-2" target="_blank" href={genScanUrl('address',text)}>{shortStr(text)}</Link>
+            <Link className="text-green-2" target="_blank" href={genScanUrl('address',text)}>{shortStr(text)}</Link>
         )
       }
     }
   ]
   const polygonscanDesc = t(
-    "You can also view raw data on the Polygon Blockchain via {{value}}. Notice polygonscan’s service may not be accessible from certain countries or regions."
+      "You can also view raw data on the Polygon Blockchain via {{value}}. Notice polygonscan’s service may not be accessible from certain countries or regions."
   ).replace(
-    "{{value}}",
-    `<span class="text-green-2">polygonscan</span>`
+      "{{value}}",
+      `<a href="https://polygon.technology/" target="_blank" rel="noreferrer" class="text-green-2">polygonscan</a>`
   )
   return (
-    <div className="bg-gray-16 flex-1 flex flex-col w-full min-h-fit text-black">
-      <HeaderLayout className="" tits={t("Trust Label NFT Viewer")}>
+      <div className="bg-gray-16 flex-1 flex flex-col w-full min-h-fit text-black">
+        {/*<HeaderLayout className="" tits={t("Trust Label NFT Viewer")}>*/}
+        <header className="bg-green-2 text-white flex items-center h-[4.25rem]">
+            <SVGAICD className="h-[2.25rem] mo:h-[1.75rem] fill-white ml-[3.125rem] mo:ml-4" />
+            <div className="w-[6rem] ml-4 mo:ml-3 text-base mo:text-[0.8rem] mo:leading-[1.125rem] mo:w-[5rem]">
+                Trust Label
+                NFT Viewer
+            </div>
+        </header>
         {loading ? (
-          <Loading />
+            <Loading />
         ) : (
-          <div className="w-full p-5 max-w-[1480px] mx-auto mo:p-0">
-            <div className="flex mo:flex-col">
-              <div className="bg-white flex justify-center px-10 flex items-center rounded-lg mo:h-[21rem] mo:px-0">
-                <ProductQrcode className="" />
-              </div>
-              <div className="flex flex-col flex-1 ml-5 mo:ml-0 mo:mt-5">
-                <div className="flex mb-5">
-                  <VscVerified className="text-3xl mr-2 mo:mt-[-0.45rem] mo:text-[2.8rem]" />
-                  <div className="flex text-lg mo:flex-col">
-                    <div className="flex flex-col">
-                      <span className="w-full">
+            <div className="w-full p-5 max-w-[1480px] mx-auto mo:p-0">
+              <div className="flex mo:flex-col">
+                <div className="bg-white flex justify-center px-10 flex items-center rounded-lg mo:h-[21rem] mo:px-0">
+                  <ProductQrcode className="" />
+                </div>
+                <div className="flex flex-col flex-1 ml-5 mo:ml-0 mo:mt-5">
+                  <div className="flex mb-5">
+                    <VscVerified className="text-3xl mr-2 mo:mt-[-0.45rem] mo:text-[2.8rem]" />
+                    <div className="flex text-lg mo:flex-col">
+                      <div className="flex flex-col">
+                      <span className="w-full font-bold">
                         {t('Automotive Carbon Footprint Trust Label')}
                         {
-                          isMobile && <span className="font-medium ml-3">#1940327340</span>
+                            isMobile && <span className="font-medium ml-3">#1940327340</span>
                         }
                       </span>
-                      {
-                        !isMobile && <p>#1940327340</p>
-                      }
+                        {
+                            !isMobile && <p>#1940327340</p>
+                        }
+                      </div>
+                      <span className="text-sm ml-3 mo:ml-0 mt-[0.3rem] mo:mt-2 mo:text-gray-6">{t('Certified by AIAG')}</span>
                     </div>
-                    <span className="text-sm ml-3 mo:ml-0 mt-[0.3rem] mo:mt-2 mo:text-gray-6">{t('Certified by AIAG')}</span>
+                  </div>
+                  <div className="bg-white px-12 py-5 rounded-lg mo:pl-10 mo:pr-3">
+                    <h5 className="text-xl mb-3.5 font-bold mo:text-lg">{t('Label Details')}</h5>
+                    <CardInfo data={value} />
                   </div>
                 </div>
-                <div className="bg-white px-12 py-5 rounded-lg mo:pl-10 mo:pr-3">
-                  <h5 className="text-xl mb-3.5 font-bold mo:text-lg">{t('Label Details')}</h5>
-                  <CardInfo data={value} />
+              </div>
+
+              <div className="bg-white mt-5 px-8 py-5 rounded-lg mo:px-4">
+                <h3 className="font-bold">{t('Item Activity on Blockchain')}</h3>
+                <div className="w-full overflow-hidden overflow-x-auto mo:pb-5">
+                  <Table className="mt-5 mo:w-[52rem]" columns={columns} data={value.activityList} />
                 </div>
               </div>
-            </div>
-
-            <div className="bg-white mt-5 px-8 py-5 rounded-lg mo:px-4">
-              <h3 className="font-bold">{t('Item Activity on Blockchain')}</h3>
-              <div className="w-full overflow-hidden overflow-x-auto mo:pb-5">
-                <Table className="mt-5 mo:w-[52rem]" columns={columns} data={value.activityList} />
+              <div className="bg-white mt-5 px-8 py-5 rounded-lg leading-[1.8rem] mo:leading-[1.6875rem] mo:px-4 mo:text-[.9375rem]">
+                <SVGAICD fill="#227A30" className="w-[6.125rem] mb-5" />
+                <p className="font-bold">
+                  {t('Database powered by:')}
+                </p>
+                <p className="text-green-2">
+                  <Link href="https://aicd.gtech.world/" target="_blank">{t('Automotive Industry Carbon Database')}</Link>
+                </p>
+                <p>
+                  {t('AICD is the global, industry-level database designed for long-term carbon emission performance traceability and visibility under the AIAG Carbon Initiative. AICD offers public view mode and supply chain view mode (Traceability).')}
+                </p>
+              </div>
+              <div className="bg-white mt-5 px-8 py-5 rounded-lg leading-[1.8rem] mo:leading-[1.6875rem] mo:px-4 mo:text-[.9375rem]">
+                <SVGPolygon className="mb-5" />
+                <p className="font-bold">
+                  {t('Blockchain powered by:')}
+                </p>
+                <p className="text-green-2">
+                  <Link href="https://polygon.technology/" target="_blank">{t('Polygon Blockchain')}</Link>
+                </p>
+                <p dangerouslySetInnerHTML={{ __html: polygonscanDesc }}>
+                </p>
               </div>
             </div>
-            <div className="bg-white mt-5 px-8 py-5 rounded-lg leading-[1.8rem] mo:leading-[1.6875rem] mo:px-4 mo:text-[.9375rem]">
-              <SVGAICD fill="#227A30" className="w-[6.125rem] mb-5" />
-              <p className="font-bold">
-                {t('Database powered by:')}
-              </p>
-              <p className="text-green-2">
-                <Link href="https://aicd.gtech.world/" target="_blank">{t('Automotive Industry Carbon Database')}</Link>
-              </p>
-              <p>
-                {t('AICD is the global, industry-level database designed for long-term carbon emission performance traceability and visibility under the AIAG Carbon Initiative. AICD offers public view mode and supply chain view mode (Traceability).')}
-              </p>
-            </div>
-            <div className="bg-white mt-5 px-8 py-5 rounded-lg leading-[1.8rem] mo:leading-[1.6875rem] mo:px-4 mo:text-[.9375rem]">
-              <SVGPolygon className="mb-5" />
-              <p className="font-bold">
-                {t('Blockchain powered by:')}
-              </p>
-              <p className="text-green-2">
-                <Link href="https://polygon.technology/" target="_blank">{t('Polygon Blockchain')}</Link>
-              </p>
-              <p dangerouslySetInnerHTML={{ __html: polygonscanDesc }}>
-              </p>
-            </div>
-          </div>
         )}
-      </HeaderLayout>
-    </div>
+        {/*</HeaderLayout>*/}
+      </div>
   );
 }
 
