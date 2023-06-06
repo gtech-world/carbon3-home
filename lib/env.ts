fe48a9cc7bf378aa44fdc588c0adf44e74dbcbc5
lijun4639 <346766377@qq.com>
2023-06-06 11:32:25 +0800

    updated qrcode and env

--- lib/env.ts
+++ lib/env.ts
@@ -1,4 +1,4 @@
-export const API_BASE = process.env.NEXT_PUBLIC_API_BASE || "https://api.gtech.world";
+export const API_BASE = process.env.NEXT_PUBLIC_API_BASE || "https://api.gtech-cn.co";
 export const SCAN_BASE = process.env.NEXT_PUBLIC_SCAN_BASE || "https://www.oklink.com/cn/polygon";
 export const AICP_SOLUTIONS = process.env.NEXT_PUBLIC_AICP_SOLUTIONS || "https://gtech.world/solutions/automotive";
 export const LABEL_CONTRACT = process.env.NEXT_PUBLIC_LABEL_CONTRACT || "0x7BC6afe0cDc6DE9191dfC6d68A3bad45E270F695";
