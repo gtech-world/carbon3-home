8dd43fe15b3626599d2e2c0d23c38ad7ca8aa9a0
eericxu <2681350846@qq.com>
2023-06-27 16:46:40 +0800

    fix error for model

--- components/model/typeContent.tsx
+++ components/model/typeContent.tsx
@@ -1,9 +1,9 @@
 import { Loading } from "@components/common/loading";
-import { Descriptor, ModelType } from "@lib/@types/lca";
+import { Descriptor, ModelType, NavNode } from "@lib/@types/lca";
 import { useSafe } from "@lib/hooks/useSafe";
 import { getLcaModelItem } from "@lib/http";
 import { parseRefJson } from "@lib/utils";
-import { FC, useEffect, useRef, useState } from "react";
+import { FC, useEffect, useMemo, useRef, useState } from "react";
 import { JsonView } from "./common/jsonView";
 import { useSelectNavs } from "./context";
 import { ProcessView } from "./process/processView";
@@ -34,6 +34,12 @@ function DefView(p: { json: any }) {
   );
 }
 
+function getKey(node: NavNode | undefined) {
+  if (!node) return undefined;
+  const des = node.data as Descriptor;
+  return `${node.modelType}_${des.id}`;
+}
+
 export function TypeContent(p: { id: string }) {
   const { id } = p;
   const { active } = useSelectNavs();
@@ -52,15 +58,15 @@ export function TypeContent(p: { id: string }) {
     // safe check
     if (!active || !active.data) return () => {};
     // cache load
-    const des = active.data as Descriptor;
-    const key = `${active.modelType}_${des.id}`;
+    const key = getKey(active) as string;
     setLoading(true);
     if (!TypeCache[key]) {
       // TODO replace get type data api
-      TypeCache[key] = getLcaModelItem(id, active.modelType, (active.data as Descriptor).id).then((data) => [
-        parseRefJson(data),
-        data,
-      ]);
+      TypeCache[key] = getLcaModelItem(id, active.modelType, (active.data as Descriptor).id).then((data) => {
+        const parsedData = parseRefJson(data);
+        parsedData._key = key;
+        return [parsedData, data];
+      });
     }
     TypeCache[key]
       .then((data) => {
@@ -78,6 +84,6 @@ export function TypeContent(p: { id: string }) {
   const Dom = TypeContentMap[active?.modelType || ""] || DefView;
   if (!active) return null;
   if (loading) return <Loading />;
-  if (!value) return null;
+  if (!value || getKey(active) !== value._key) return null;
   return <Dom data={TypeContentMap[active?.modelType || ""] ? value : jsonValue} json={jsonValue} />;
 }
