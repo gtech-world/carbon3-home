2d10e20a2f36c6b463c97bf0999394e1f549e3e5
eericxu <2681350846@qq.com>
2023-08-01 11:13:05 +0800

    add prettier config

--- components/common/modal.tsx
+++ components/common/modal.tsx
@@ -8,19 +8,11 @@ export const modalRootRef: MutableRefObject<HTMLDivElement | null> = {
   current: null,
 };
 
-export function ModalHeader(p: {
-  title?: string;
-  titleStyle?: string;
-  containerStyle?: string;
-  onClose?: Function;
-}) {
+export function ModalHeader(p: { title?: string; titleStyle?: string; containerStyle?: string; onClose?: Function }) {
   const { title, onClose, titleStyle, containerStyle } = p;
   return (
     <div
-      className={classNames(
-        "flex items-center justify-between pb-6 mb-6 text-xl font-bold border-b",
-        containerStyle
-      )}
+      className={classNames("flex items-center justify-between pb-6 mb-6 text-xl font-bold border-b", containerStyle)}
     >
       <span className={titleStyle}>{title}</span>
       <FiX
@@ -38,36 +30,21 @@ export function ModalFooter(p: {}) {
   return <div></div>;
 }
 
-export type ModalProps = { title?: string; onClose?: Function; outClose?: boolean } & HTMLAttributes<HTMLDivElement>;
+export type ModalProps = {
+  title?: string;
+  onClose?: Function;
+  outClose?: boolean;
+  titleStyle?: string;
+  containerStyle?: string;
+} & HTMLAttributes<HTMLDivElement>;
 
-
-export function Modal(
-  p: {
-    title?: string;
-    titleStyle?: string;
-    containerStyle?: string;
-    onClose?: Function;
-    outClose?: boolean;
-  } & HTMLAttributes<HTMLDivElement>
-) {
-  const {
-    className,
-    title,
-    onClose,
-    outClose = true,
-    titleStyle,
-    containerStyle,
-    children,
-    ...other
-  } = p;
+export function Modal(p: ModalProps) {
+  const { className, title, onClose, outClose = true, titleStyle, containerStyle, children, ...other } = p;
   const ref = useRef<HTMLDivElement>(null);
   const onClickOut = useOn((e: MouseEvent<HTMLDivElement>) => {
     e.stopPropagation();
     e.preventDefault();
-    const isChild =
-      ref.current &&
-      ref.current !== e.target &&
-      ref.current.contains(e.target as any);
+    const isChild = ref.current && ref.current !== e.target && ref.current.contains(e.target as any);
     !isChild && outClose && onClose && onClose();
   });
   if (!modalRootRef.current) return null;
@@ -75,25 +52,14 @@ export function Modal(
     <div
       {...other}
       ref={ref}
-      onClick={onClickOut}
       className={classNames(
         "fixed left-0  top-0 w-full h-full overflow-auto z-50 bg-black/25 flex justify-center items-center",
         className
       )}
+      onClick={onClickOut}
     >
-      <div
-        id="mo"
-        ref={ref}
-        className={classNames(
-          "bg-white rounded p-5 min-w-[20rem] min-h-[150] "
-        )}
-      >
-        <ModalHeader
-          title={title}
-          containerStyle={containerStyle}
-          titleStyle={titleStyle}
-          onClose={onClose}
-        />
+      <div id="mo" ref={ref} className={classNames("bg-white rounded p-5 min-w-[20rem] min-h-[150] ")}>
+        <ModalHeader title={title} containerStyle={containerStyle} titleStyle={titleStyle} onClose={onClose} />
         {children}
       </div>
     </div>,
