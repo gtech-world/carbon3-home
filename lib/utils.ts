9f1dcad806943ff6d74e0d251dcb0704d6b51595
eericxu <2681350846@qq.com>
2023-08-10 10:38:32 +0800

    fix shortStr

--- lib/utils.ts
+++ lib/utils.ts
@@ -83,7 +83,7 @@ export function sleep(t: number = 0) {
 }
 export function shortStr(str?: string, startLen: number = 6, endLen: number = 6) {
   if (!str) return "";
-  if (str.length >= startLen + endLen) return str;
+  if (str.length <= startLen + endLen) return str;
   const start = str?.substring(0, startLen) || "";
   const end = str?.substring(str.length - endLen) || "";
   return `${start}...${end}`;
