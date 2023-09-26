7c7cc0f1ff2ac4332659201da052490013e2acf9
eericxu <2681350846@qq.com>
2023-09-26 11:01:26 +0800

    fix autorefresh data for modellist and inventorylist

--- components/routes/tools/inventory.tsx
+++ components/routes/tools/inventory.tsx
@@ -218,7 +218,7 @@ export function Inventory() {
     return () => {
       stop = true;
     };
-  }, []);
+  }, [pgNum]);
 
   const unVerifier = useUnVerifier();
 
--- components/routes/tools/model.tsx
+++ components/routes/tools/model.tsx
@@ -48,7 +48,7 @@ export function Model() {
     return () => {
       stop = true;
     };
-  }, []);
+  }, [pgNum]);
 
   const columns = useMemo(
     () => [
