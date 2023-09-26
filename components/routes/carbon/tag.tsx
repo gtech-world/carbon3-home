c1ba328206961e617c17a4f3d23ccb7cca192aff
YongZL <1679963578@qq.com>
2023-09-26 16:01:20 +0800

    test

--- components/routes/carbon/tag.tsx
+++ components/routes/carbon/tag.tsx
@@ -101,13 +101,44 @@ export function Tag() {
     setTagList(res);
   };
 
+  const data = [
+    {
+      title: "2",
+      icon: {
+        key: null,
+        ref: null,
+        props: {
+          className: "w-[2.75rem]",
+        },
+        _owner: null,
+        _store: {},
+      },
+      by: "2023年09月15日签发 by AIAG",
+      id: "bd7d4203-60d0-49de-b00e-5946cfe0cd04-1695016205",
+      qrcodeDisable: false,
+      link: [
+        {
+          text: "标签信息",
+          href: "/car?vin=bd7d4203-60d0-49de-b00e-5946cfe0cd04-1695016205",
+        },
+        {
+          text: "在区块链浏览器查看",
+          target: "_blank",
+          href: "/blockchain?tokenId=3000000",
+        },
+      ],
+      tokenId: 3000000,
+      qrCode: " https://aicp.gtech.world/car?vin=bd7d4203-60d0-49de-b00e-5946cfe0cd04-1695016205",
+    },
+  ];
+
   useEffect(() => {
     getTagList();
   }, []);
   return (
     <CarbonLayout className="h-full bg-gray-16">
       <div className="flex flex-wrap">
-        {tagList?.records.map((v: any, i) => {
+        {data.map((v: any, i) => {
           return <Card key={`tagData${i}`} data={v} />;
         })}
       </div>
--- lib/services/carbonTag.ts
+++ lib/services/carbonTag.ts
@@ -11,14 +11,15 @@ export async function getCarbonTagList(pgNum: number = 0) {
 }
 
 export async function getSbtUUIDInfo(vin: string | number) {
-  const res = await axios.get<Res<SbtTokenController.TagList>>(createUrl(`/api/sbt/token/uuid/${vin}`), authConfig());
+  const res = await axios.get<Res<SbtTokenController.TagList>>(
+    `https://api-v2.gtech.world/api/sbt/token/uuid/bd7d4203-60d0-49de-b00e-5946cfe0cd04-1695016205`,
+  );
   return getData(res);
 }
 
 export async function getSbtDetail(tokenId: string | number) {
   const res = await axios.get<Res<SbtTokenController.SbtNftList>>(
-    createUrl(`/api/sbt/token/nft/${tokenId}`),
-    authConfig(),
+    `https://api-v2.gtech.world/api/sbt/token/nft/${3000000}`,
   );
   return getData(res);
 }
