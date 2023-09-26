165d2e213576db790909a7d36494e04122788650
eericxu <2681350846@qq.com>
2023-09-26 16:36:09 +0800

    add verify params for verify

--- lib/http.ts
+++ lib/http.ts
@@ -292,7 +292,17 @@ export async function updateVerifyRecord(id: number, config: Partial<CreateVerif
   return getData(res);
 }
 
-export async function verifyVerifyRecord(id: number, fileList: number[], state: boolean = false) {
-  const res = await axios.post<Res<any>>(creatUrl(`/api/verifyRecord/${id}/verify`), { fileList, state }, authConfig());
+export async function verifyVerifyRecord(
+  id: number,
+  params: {
+    functionalUnit?: string;
+    evaluationBoundary?: string;
+    evaluationBasis?: string;
+    evaluationExpireTime?: string;
+    fileList: number[];
+    state: boolean;
+  },
+) {
+  const res = await axios.post<Res<any>>(creatUrl(`/api/verifyRecord/${id}/verify`), { ...params }, authConfig());
   return getData(res);
 }
